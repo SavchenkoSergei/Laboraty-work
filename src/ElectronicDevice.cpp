df487439fb607044adddfd92383e1a2027e34d52
SergeiSavchenko <SergeiSavchenko2@yandex.by>
2026-09-07 20:35:18 +0300

    Переоформил сеттеры и геттеры.

--- src/ElectronicDevice.cpp
+++ src/ElectronicDevice.cpp
@@ -8,19 +8,43 @@ ElectronicDevice::ElectronicDevice(std::string_view devType, std::string_view de
     price(initialPrice), warrantyMonths(initialWarranty), extraSpec(initialExtra) {
 }
 
-std::string ElectronicDevice::getType() const { return type; }
-std::string ElectronicDevice::getModel() const { return model; }
-std::string ElectronicDevice::getManufacturer() const { return manufacturer; }
-double ElectronicDevice::getPrice() const { return price; }
-int ElectronicDevice::getWarrantyMonths() const { return warrantyMonths; }
-std::string ElectronicDevice::getExtraSpec() const { return extraSpec; }
+std::string ElectronicDevice::getType() const { 
+    return type; 
+}
+std::string ElectronicDevice::getModel() const { 
+    return model; 
+}
+std::string ElectronicDevice::getManufacturer() const { 
+    return manufacturer; 
+}
+double ElectronicDevice::getPrice() const { 
+    return price; 
+}
+int ElectronicDevice::getWarrantyMonths() const { 
+    return warrantyMonths; 
+}
+std::string ElectronicDevice::getExtraSpec() const { 
+    return extraSpec; 
+}
 
-void ElectronicDevice::setType(std::string_view newType) { type = newType; }
-void ElectronicDevice::setModel(std::string_view newModel) { model = newModel; }
-void ElectronicDevice::setManufacturer(std::string_view newManufacturer) { manufacturer = newManufacturer; }
-void ElectronicDevice::setPrice(double newPrice) { price = newPrice; }
-void ElectronicDevice::setWarrantyMonths(int months) { warrantyMonths = months; }
-void ElectronicDevice::setExtraSpec(std::string_view newExtra) { extraSpec = newExtra; }
+void ElectronicDevice::setType(std::string_view newType) { 
+   type = newType; 
+}
+void ElectronicDevice::setModel(std::string_view newModel) { 
+   model = newModel; 
+}
+void ElectronicDevice::setManufacturer(std::string_view newManufacturer) { 
+   manufacturer = newManufacturer; 
+}
+void ElectronicDevice::setPrice(double newPrice) { 
+   price = newPrice; 
+}
+void ElectronicDevice::setWarrantyMonths(int months) { 
+    warrantyMonths = months; 
+}
+void ElectronicDevice::setExtraSpec(std::string_view newExtra) { 
+    extraSpec = newExtra; 
+}
 
 void ElectronicDevice::printInfo() const {
     std::cout << "[" << type << "] "
